c655ece76ac2581fa7b0f250c4bdbf057a768f09
Kyle <kyle@HuangdeMacBook-Pro.local>
2016-06-30 00:41:10 +0800

    first commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+ platform :ios, '8.0'
+
+target 'SecoundFblogin' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SecoundFblogin
+
+pod 'FBSDKLoginKit'
+
+end
---
+++ SecoundFblogin/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  SecoundFblogin
+//
+//  Created by Kyle on 6/29/16.
+//  Copyright © 2016 Alphacamp. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+
+class ViewController: UIViewController,FBSDKLoginButtonDelegate {
+
+    @IBOutlet weak var fbLoginButton: FBSDKLoginButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        fbLoginButton.delegate = self
+        fbLoginButton.readPermissions = ["public_profile", "email", "user_friends"]
+
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+        
+    }
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+    }
+}
